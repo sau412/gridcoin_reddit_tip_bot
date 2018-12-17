162ba4da4cbeb5a1d5bf4313fad2ee2c9a8b9627
Tsarev Vladimir <sau412@gmail.com>
2018-12-17 18:10:16 +0300

    Create settings.php

---
+++ settings.php
@@ -0,0 +1,19 @@
+<?php
+// Settings
+
+// HTTP auth
+$reddit_http_login="";
+$reddit_http_password="";
+
+// Reddit login and password
+$reddit_username="";
+$reddit_password="";
+
+$reddit_secret="";
+
+$reddit_useragent="Linux:grc_tip_bot:v_0 (by /u/sau412)";
+
+// Allowed subreddits list
+$reddit_allowed_subreddit_array=array();
+
+?>
