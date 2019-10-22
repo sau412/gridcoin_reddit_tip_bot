9824c4f0c9175396b1357a86b7d102892b955fe9
sau412 <sau412@protonmail.com>
2019-10-22 15:42:35 +0300

    Main page with stats

---
+++ web/index.php
@@ -0,0 +1,62 @@
+<?php
+require_once("../lib/settings.php");
+require_once("../lib/db.php");
+
+echo "<h1>Gridcoin Reddit Tip Bot Stats</h1>\n";
+
+$users_total=db_query_to_variable("SELECT count(*) FROM `users`");
+$users_positive=db_query_to_variable("SELECT count(*) FROM `users` WHERE `balance`>0");
+
+$tips_count=db_query_to_variable("SELECT count(*) FROM `withdrawals` WHERE `type`='tip'");
+$topics=db_query_to_variable("SELECT count(*) FROM `posts`");
+$messages=db_query_to_variable("SELECT count(*) FROM `messages`");
+
+echo "<h2>Stats</h2>\n";
+
+echo <<<_END
+<table>
+<tr><th>Total users</th><td>$users_total</td>
+<tr><th>Users with balance</th><td>$users_positive</td>
+<tr><th>Tips count</th><td>$tips_count</td>
+<tr><th>Reddit posts</th><td>$topics</td>
+<tr><th>Reddit messages</th><td>$messages</td>
+</table>
+
+_END;
+
+
+echo "<h2>Last 10 visible topics</h2>\n";
+
+$topics_data=db_query_to_array("SELECT `subreddit`,`post_id`,`author`,`comments` FROM `posts` ORDER BY `timestamp` DESC LIMIT 10");
+
+echo <<<_END
+<table>
+<tr><th>Subreddit</th><th>Post</th><th>Author</th><th>Comments</th></tr>
+_END;
+
+foreach($topics_data as $row) {
+	$subreddit=$row['subreddit'];
+	$post_id=$row['post_id'];
+	$author=$row['author'];
+	$comments=$row['comments'];
+
+	$author_html=htmlspecialchars($author);
+	$subreddit_link="<a href='https://reddit.com/$subreddit'>$subreddit</a>";
+	$post_link="<a href='https://reddit.com/$subreddit/$post_id'>$post_id</a>";
+	echo "<tr><td>$subreddit_link</td><td>$post_link</td><td>$author_html</td><td>$comments</td></tr>\n";
+}
+
+echo "</table>\n";
+
+echo "<h2>Last 20 visible messages</h2>\n";
+
+$messages_data=db_query_to_array("SELECT `subreddit`,`post_id`,`message_id`,`message`,`author`,`reply` FROM `posts` ORDER BY `timestamp` DESC LIMIT 10");
+
+echo <<<_END
+<table>
+<tr><th>Subreddit</th><th>Message</th><th>Author</th><th>Reply</th></tr>
+_END;
+
+echo "</table>\n";
+
+?>
